096aa2f057f05e7edfb4d1b7c0b725e5d55755a3
nkarwa <43045534+nkarwa@users.noreply.github.com>
2020-06-10 09:33:27 +0530

    updated

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -26,7 +26,7 @@ public class NewTest {
 			//System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
 			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mitesh\\Downloads\\chromedriver_win32\\chromedriver.exe");
 			//driver = new ChromeDriver();  
-			File file = new File("C:\gecko\geckodriver.exe");
+			File file = new File("C:\\gecko\\geckodriver.exe");
 			System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());			
 			
 			
